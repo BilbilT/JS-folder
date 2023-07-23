be34fa22a107d4424bcb6bfd1e76222d61326bed
Bilbil <bilbiltorayeva@gmail.com>
2023-07-23 20:13:04 +0100

    Added conditional statements

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
+// let button = document.querySelector(".show-cat");
+// //console.log(button);
+
+// let cat = document.querySelector(".cat");
+
+// button.addEventListener("click", function () {
+//   button.innerText = "It's working";
+//   cat.classList.add("show");
+// });
+
 let button = document.querySelector(".show-cat");
-//console.log(button);
 
 let cat = document.querySelector(".cat");
-//console.log(cat);
 
 button.addEventListener("click", function () {
-  button.innerText = "It's working";
-  cat.classList.add("show");
+  if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+  } else {
+    cat.classList.add("show");
+    button.innerText = "Shoo, cat!";
+    button.classList.remove("disappear");
+  }
+  //cat.classList.add("show");
 });
